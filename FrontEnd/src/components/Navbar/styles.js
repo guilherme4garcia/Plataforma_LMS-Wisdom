dcf375e6fd4cc0a74a15edcad3c2a6b07ad9b831
Luis Verola <luisgabriel.verola@gmail.com>
2021-03-24 21:30:22 -0300

    feature: Adiçao da Navbar com conteudo e landing page

--- FrontEnd/src/components/Navbar/styles.js
+++ FrontEnd/src/components/Navbar/styles.js
@@ -1,3 +1,28 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background-color: var(--color-white);
+`;
+
+export const Content = styled.div`
+  max-width: 1128px;
+  margin: 0 auto;
+
+  padding: 0.75rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const MenuNavBarLeft = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MenuNavBarRight = styled.aside`
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+`;
--- FrontEnd/src/pages/LandingPage/styles.js
+++ FrontEnd/src/pages/LandingPage/styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div``;
 
-`;
+export const About = styled.div``;
+
+export const WhoWeAre = styled.div``;
+
+export const Contact = styled.div``;
